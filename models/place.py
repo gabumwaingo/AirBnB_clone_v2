fa684b470ec0082e609e2b70101800b9cb6a82f3
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-06-21 12:50:24 -0700

    Updates

--- models/place.py
+++ models/place.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
 
 
 class Place(BaseModel, Base):

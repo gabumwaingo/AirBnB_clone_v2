3458c91e5a4c3a51ca4d1194e2ac198bdacd20f7
gabumwaingo <gabumwaingo@gmail.com>
2023-07-07 01:26:57 +0000

    Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,38 @@
 #!/usr/bin/env bash
+# Install Nginx if not already exist and create inexistent files
 
-# Install Nginx if not already installed
-if ! [ -x "$(command -v nginx)" ]; then
-    sudo apt-get update
-    sudo apt-get -y install nginx
-fi
+apt-get update
+apt-get install -y nginx
 
-# Create necessary folders if they don't exist
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Create a test HTML file
-sudo echo "<html><head></head><body>Testing Nginx configuration for web_static deployment</body></html>" | sudo tee /data/web_static/releases/test/index.html
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-# Create symbolic link
-sudo rm -rf /data/web_static/current
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-# Give ownership to ubuntu user and group
-sudo chown -R ubuntu /data/
-sudo chgrp -R ubuntu /data/
-# Update Nginx configuration
-sudo sed -i '37i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+    location /hbnb_static {
+	alias /data/web_static/current;
+	index index.html index.htm;
+    }
 
-# Restart Nginx
-sudo service nginx restart
+    location /redirect_me {
+	return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart

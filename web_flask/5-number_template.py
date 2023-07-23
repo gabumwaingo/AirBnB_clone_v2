1757fbecf7651a9854abc09fe71d4c0ace94a9c0
gabumwaingo <gabumwaingo@gmail.com>
2023-07-23 20:13:23 +0000

    Task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Starts a flask web app with four routes """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+	return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+	return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+	""" replaces text with a variable """
+	text = text.replace('_', ' ')
+	return 'C {}'.format(text)
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py_text(text = "is cool"):
+	""" replaces text with a variable with a default """
+	text = text.replace('_', ' ')
+	return "Python {}".format(text)
+
+@app.route('/number/<int:n>')
+def number_text(n):
+	""" replace n with an int only if n is an int """
+	n = str(n)
+	return '{} is a number'.format(n)
+
+@app.route('/number_template/<int:n>',strict_slashes=False)
+def number_template(n):
+	""" displays html if n is an int """
+	n = str(n)
+	return render_template('5-number.html', n=n) 
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5000)

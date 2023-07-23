5a05eb9f21d192f79997eeb49974a7b21c6eb3cd
gabumwaingo <gabumwaingo@gmail.com>
2023-07-23 22:05:35 +0000

    Task 7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -57,3 +57,7 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(session)
+
+    def close(self):
+        """ call close on private session. """
+        self.__session.close()

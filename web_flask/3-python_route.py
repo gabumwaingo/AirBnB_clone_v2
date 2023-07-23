e43b61f2a253005b5827e4b79af3acf9de3b3582
gabumwaingo <gabumwaingo@gmail.com>
2023-07-23 19:32:03 +0000

    Task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Starts a flask web app with four routes """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+	return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+	return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+	""" replaces text with a variable """
+	text = text.replace('_', ' ')
+	return 'C {}'.format(text)
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py_text(text = "is cool"):
+	""" replaces text with a variable with a default """
+	text = text.replace('_', ' ')
+	return "Python {}".format(text)
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5000)
